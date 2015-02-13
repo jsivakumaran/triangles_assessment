e31f58138de121d4a5448b5231552f1e98239216
Jay <nayagan@gmail.com>
2015-02-13 11:13:56 -0800

    Add front-end plus a little refactoring of back-end code

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,30 @@
 var triangle = function(side1, side2, side3){
   if(side1>=(side2+side3) || side2>=(side1+side3) || side3>=(side1+side2)){
-    return "false"
-  }
-
-  if(side1 === side2 && side2 === side3){
-    return "equilateral";
-  }
-
-  if(side1===side2 || side2===side3 || side1===side3){
-    return "isosceles";
-  }
-
-  if(side1!=side2 && side2!=side3 && side1!=side3){
-    return "scalene";
+    return "non-triangle shape.";
+  }else if(side1 === side2 && side2 === side3){
+    return "equilateral.";
+  }else if(side1===side2 || side2===side3 || side1===side3){
+    return "isosceles.";
+  }else if (side1!=side2 && side2!=side3 && side1!=side3){
+    return "scalene.";
   }
 };
+
+
+$(function(){
+  $("#dimensions").submit(function(event){
+     userInputS1 = parseInt($(length1).val());
+     userInputS2 = parseInt($(length2).val());
+     userInputS3 = parseInt($(length3).val());
+     $(".results").fadeOut("fast");
+
+     whatType = triangle(userInputS1, userInputS2, userInputS3);
+
+    $("span.triangleType").text(whatType);
+
+    $(".results").toggle(function(){
+      $(this).fadeIn("slow");
+    });
+    event.preventDefault();
+  });
+});
