a6efb2e4cfb70bf5bf97dc703432c0632ff25236
Jay <nayagan@gmail.com>
2015-02-13 10:00:12 -0800

    Fix bug relating to mislabelling equilateral as isosceles and add two additional specs

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,8 @@ var triangle = function(side1, side2, side3){
   }
 
   if(side1 === side2 && side2 === side3){
-    return "isosceles";
+    return "equilateral";
   }
 
-  
+
 };
--- spec/specs.js
+++ spec/specs.js
@@ -3,6 +3,12 @@ describe('triangles', function(){
     expect(triangle(2, 2, 8)).to.equal("false");
   });
   it("returns equilateral when all sides are equal", function(){
-    expect(triangle(3,3,3)).to.equal("isosceles");
+    expect(triangle(3,3,3)).to.equal("equilateral");
+  });
+  it("returns isosceles when exactly two sides are equal", function(){
+    expect(triangle(2,2,3)).to.equal("isosceles");
+  });
+  it("returns scalene when no sides are equal", function(){
+    expect(triangle(4,5,6)).to.equal("scalene");
   });
 });
